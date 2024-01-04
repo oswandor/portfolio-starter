ac13596fee9f124eaa428720fd264dfcdfe3a705
RonalOswaldoGonzalez <oswandor26@gmail.com>
2024-01-04 16:58:23 -0600

    Contact add styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React from "react";
+//countup
+import CountUp from "react-countup";
+
+import { useInView } from "react-intersection-observer";
+
+import { motion } from "framer-motion";
+
+import { fadeIn } from "../variants";
 
 const Contact = () => {
-  return <div className='section' id='contact'>Contact</div>;
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+
+  return (
+    <section className="section" id="contact" ref={ref}>
+      <div className="container mx-auto">
+        {/*image*/}
+        <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top ">img</div>
+
+        {/*text*/}
+        <div>
+
+          
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
