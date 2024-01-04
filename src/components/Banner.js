9aa49273d7b7f118f9c33e418a9659ce8f1bf5b6
RonalOswaldoGonzalez <oswandor26@gmail.com>
2024-01-04 15:05:16 -0600

    effecto motion

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -17,11 +17,11 @@ const Banner = () => {
       <div className="container mx-auto  sm:mt-auto">
         <div className="flex flex-col gap-y-8 lg:flex-row">
           <div className="flex-1 text-center font-secondary  lg:text-left">
-            <h1 className="text-[55px] font-bold leading-[0.8] lg:text-[110px]">
+            <motion.h1 variants={fadeIn('up', 0.3)} initial='hidden' whileInView={'show'} viewport={{once: false , amount: 0.7}}   className="text-[55px] font-bold leading-[0.8] lg:text-[110px]">
               Ronal <span>Gonzalez</span>
-            </h1>
+            </motion.h1>
 
-            <div className="mb-6 text-[30px] lg:text-[60px] font-secondary font-semibold  uppercase leading-[1]">
+            <motion.div variants={fadeIn('up', 0.3)} initial='hidden' whileInView={'show'} viewport={{once: false , amount: 0.7}}   className="mb-6 text-[30px] lg:text-[60px] font-secondary font-semibold  uppercase leading-[1]">
               <span className="mr-4">I am a</span>
 
               <TypeAnimation
@@ -38,7 +38,7 @@ const Banner = () => {
                 wrapper="span"
                 repeat={Infinity}
               ></TypeAnimation>
-            </div>
+            </motion.div>
 
             <p className="mb-8  max-w-lg mx-auto lg:mx-0">
               Soy desarrollador Full Stack altamente competente con experiencia
@@ -71,7 +71,7 @@ const Banner = () => {
             </div>
           </div>
 
-          <div className="sm:content-center">
+          <div className="sm:content-center hidden lg:flex flex-1 max-w-[320px] lg:max-w-[382px] mx-auto">
             <img src={Image}></img>
           </div>
         </div>
